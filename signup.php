043a1e286186a62e4dead01444b969efb0707c16
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-04-10 17:14:06 -0300

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/signup.css?v=<?php echo time(); ?>">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <section>
+        <div class="my">
+            <div class="cont">
+            <img src="styles/assets/nasa-logo.png">
+            <form action="">
+                <input type="text" name="user" id="username" placeholder="Username">
+                <input type="password" name="pass" id="password" placeholder="Password">
+                <input type="text" name="user" id="telefone" placeholder="Telefone">
+                <input type="email" name="e-mail" id="e-mail" placeholder="e-mail">
+                <input type="submit" value="Press" id="sub">
+            </form>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
+</body>
+</html>
