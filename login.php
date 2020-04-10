d5205219fa8f58c657ed0c2f585bc2d45c6216f0
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-04-10 17:07:52 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/login.css?v=<?php echo time(); ?>">
     <title>Document</title>
 </head>
+
 <body>
-    
+
+    <section>
+        <div class="my">
+            <div class="cont">
+            <img src="styles/assets/nasa-logo.png">
+            <form action="">
+                <input type="text" name="user" id="username" placeholder="Username">
+                <input type="password" name="pass" id="password" placeholder="Password">
+                <input type="submit" value="Press" id="sub">
+            </form>
+            <div class="forgot">
+                <a href="signin.php"><p>Don't have an account?</p></a>
+            </div>
+            </div>
+        </div>
+    </section>
+
 </body>
+
 </html>
