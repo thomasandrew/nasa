c21c89c1c620b150b7b178cf810d978a5d4c3887
Thomas <53497553+thomasandrew@users.noreply.github.com>
2020-04-17 12:52:34 -0300

    Add files via upload

--- php/welcome.php
+++ php/welcome.php
@@ -1,45 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="../js/main.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
+<style>
+    #main {
+        max-width: 1080px; 
+        margin:0 auto;
+    }
 
-<body onload="screenshoots.picture1()">
+    img {
+        max-width: 100%; 
+    }
 
-    <p>Welcome</p>
-    <img src="../js/images/one.jpg" alt="slide show" id="banner">
+</style>
+<body onload="slide1()">
 
+    <p> Welcome </p>    
+
+    <div id="main">
+        <img src="../js/images/one.jpg" alt="slide show" id="banner">
+    </div>
     <script>
-        class Screenshoots {
-            constructor(photos) {
-                this.photos = photos;
-            }
+         var intervalo = 3000;
 
-            picture1() {
-                document.getElementById('banner').src = "../js/images/one.jpg"
-                setTimeout(this.picture2(), this.photos)
-            }
+         function slide1() {
+            document.getElementById('banner').src = "../js/images/one.jpg"
+            setTimeout("slide2()",intervalo)
+         }   
+         
+         function slide2() {
+            document.getElementById('banner').src = "../js/images/two.jpg"
+            setTimeout("slide3()",intervalo)
+         }
 
-            picture2() {
-                document.getElementById('banner').src = "../js/images/two.jpg"
-                setTimeout(this.picture1(), this.photos)
-            }
+         function slide3() {
+            document.getElementById('banner').src = "../js/images/three.jpg"
+            setTimeout("slide1()",intervalo)
+         }
 
-            picture3() {
-                document.getElementById('banner').src = "../js/images/three.jpg"
-                setTimeout(this.picture1(), this.photos)
-            }
-        }
-
-        const screenshoots = new Screenshoots(3000)
-        screenshoots.picture1()
     </script>
-
-
 </body>
-
 </html>
